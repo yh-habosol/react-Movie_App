960a5543b4edaf3111219f3ee8e9fa62ef72783c
yh-habosol <k0912h@naver.com>
2020-07-21 09:07:03 +0900

    make first Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Potato from "./Potato";
 
 function App() {
-  return <div className="App">Hello!!</div>;
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Potato />
+    </div>
+  );
 }
 
 export default App;
