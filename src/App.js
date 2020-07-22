d5964d99dd063479f2b46dde083f1e38c2597be6
yh-habosol <k0912h@naver.com>
2020-07-22 23:18:07 +0900

    Component lifeCycle

--- src/App.js
+++ src/App.js
@@ -14,7 +14,18 @@ class App extends React.Component {
     this.setState((current) => ({ count: current.count - 1 }));
   };
 
+  componentDidMount() {
+    console.log("Component rendered");
+  }
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+  componentWillUnmount() {
+    console.log("Goodbye, cruel world");
+  }
+
   render() {
+    console.log("I'm rendering");
     return (
       <div>
         <h1>The number is : {this.state.count}</h1>
