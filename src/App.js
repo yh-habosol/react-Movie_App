91a229771496b6e5fb746f98bc1d25a85258ed8c
yh-habosol <k0912h@naver.com>
2020-07-25 12:46:45 +0900

    building the router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
 
 function App() {
-  return <span>Something</span>;
+  return (
+    <HashRouter>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
